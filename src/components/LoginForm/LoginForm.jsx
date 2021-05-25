f00f8f00f0a7a3c72301c80c14a79559f0bae7ee
Chloe Everson <chloeliz93@gmail.com>
2021-05-24 19:07:05 -0500

    login form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -2,11 +2,45 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import {useSelector} from 'react-redux';
 
+import {Button, Grid, Container, Typography, TextField, Card, CardHeader, CardContent, makeStyles, createMuiTheme, ThemeProvider} from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+
+const useStyles = makeStyles({
+
+  login: {
+      marginTop: 100,
+    
+  },
+  btn: {
+      marginTop: 20,
+      marginBottom: 15,
+  },
+  signin: {
+      marginBottom: 10
+  },
+  password: {
+    marginTop: 10,
+  }
+
+})
+const theme = createMuiTheme({
+  palette: {
+      primary: {
+          main: '#F49D0C'
+      }
+  }
+})
+
+
 function LoginForm() {
+
+  const classes=useStyles();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const errors = useSelector(store => store.errors);
   const dispatch = useDispatch();
+
+ 
 
   const login = (event) => {
     event.preventDefault();
@@ -25,10 +59,36 @@ function LoginForm() {
   }; // end login
 
   return (
+    <>
+    <form>
+    <Container align="center" className={classes.login}>
+      <Grid noValidate autoComplete="off" onSubmit={login} lg={2} xs={6} sm={3} md={3}>
+        {/* <Card>
+          <CardHeader> */}
+          <Typography variant="h5" className={classes.signin}>Sign In</Typography>
+          {errors.loginMessage && (
+        <h3 className="alert" role="alert">
+          {errors.loginMessage}
+        </h3>
+      )}
+          {/* </CardHeader>
+          <CardContent> */}
+          <TextField required id="standard-required" label="Username" color="secondary" value={username}
+            onChange={(event) => setUsername(event.target.value)}/>
+          <TextField className={classes.password} required label="Password" variant="standard" color="secondary" value={password} 
+            type="password" onChange={(event) => setPassword(event.target.value)}/>
+          <ThemeProvider theme={theme}>
+            <Button variant="contained" className={classes.btn} color="primary" align="center" onClick={login}>Sign In</Button>
+            </ThemeProvider>
+  
+          {/* </CardContent>
+        </Card> */}
+      </Grid>
+    </Container>
+    </form>
+  
 
-
-    
-    <form className="formPanel" onSubmit={login}>
+    {/* <form className="formPanel" onSubmit={login}>
       <h2>Login</h2>
       {errors.loginMessage && (
         <h3 className="alert" role="alert">
@@ -62,7 +122,8 @@ function LoginForm() {
       <div>
         <input className="btn" type="submit" name="submit" value="Log In" />
       </div>
-    </form>
+    </form> */}
+    </>
   );
 }
 
